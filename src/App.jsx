5185b451d0d1b8895eb4e0e07ef752b0dc4214c7
createdbyamir <amirabdulla853@gmail.com>
2026-07-17 23:55:49 +0100

    style: update favicon and add project logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Nasa from './Nasa'
+import logo from './assets/logo.png'
 
 function App() {
 
   return (
     <>
+      <img src={logo} alt="logo" className="h-[50ox] w-[50px]"/>
       <Nasa />
     </>
   )
