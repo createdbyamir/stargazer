bd518c03c11e74aa4572236eb794e193c775c6a3
createdbyamir <amirabdulla853@gmail.com>
2026-07-17 18:20:29 +0100

    refactor: center loading indicator and simplify JSX

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -3,13 +3,14 @@ import { Atom } from 'react-loading-indicators';
 export default function LoadingAtom() {
     return (
         <>
-            <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-center justify-center h-screen">
                 <Atom 
-                    color="#2f5d2f" 
-                    size="medium" 
-                    text="Loading Stargazer" 
-
+                    color="#3d1148" 
+                    size="large" 
                 />
+                <p className="mt-2 text-[#3d1148] text-xl">
+                    Launching Stargazer...
+                </p>
             </div>
         </>
     )
