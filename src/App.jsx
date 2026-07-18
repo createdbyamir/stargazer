b92b3d23d56fa773f2cd8c1889f1daa351242bf6
createdbyamir <amirabdulla853@gmail.com>
2026-07-18 10:59:00 +0100

    style: remove project logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import Nasa from './Nasa'
-import logo from './assets/logo.png'
 
 function App() {
 
   return (
     <>
-      <img src={logo} alt="logo" className="h-[50ox] w-[50px]"/>
       <Nasa />
     </>
   )
