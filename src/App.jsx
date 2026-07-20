a31849b96a2a2eb48d90e67772065ed4ba980c0a
createdbyamir <amirabdulla853@gmail.com>
2026-07-20 09:45:31 +0100

    feat: add application header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Nasa from './Nasa'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
+    <> 
+      <Header />
       <Nasa />
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <>
+            <h1>Stargazer</h1>
+            <p>NASA Astronomy Picture of the Day - curated images from across the cosmos</p>
+        </>
+    )
+}
