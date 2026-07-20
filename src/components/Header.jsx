119f2ac74c3c32398bc2819fdbdbb518e7be0721
createdbyamir <amirabdulla853@gmail.com>
2026-07-20 11:55:09 +0100

    style: improve header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@
 export default function Header() {
     return (
         <>
-            <h1>Stargazer</h1>
-            <p>NASA Astronomy Picture of the Day - curated images from across the cosmos</p>
+            <div className="h-[300px] flex flex-col justify-center  items-center bg-[#060e1d] text-[#44546b] text-lg" >
+                <h1 className="text-5xl pb-4 uppercase tracking-widest">Stargazer</h1>
+                <p>NASA Astronomy Picture of the Day - curated images from across the cosmos</p>
+            </div>
         </>
     )
 }
